13c251e1cca7ab56df4685ad6dbcd84c61380d31
Viktor Pavlyshyn <viktor_pavlyshyn@epam.com>
2023-04-06 15:50:45 +0300

    add folder built into .gitignore

--- built/data/Urls.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Urls {
-    constructor() {
-        this.baseUrl = "https://jsonplaceholder.typicode.com";
-    }
-}
-exports.default = new Urls();
